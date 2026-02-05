79cad587290baa0471b7bd858ab4d0582177c4e1
anagjurchinova <a.gjurcinova9@gmail.com>
2026-02-05 07:20:34 +0100

    Added producer scaling and redis for SocketIO

--- app/socketio_ext.py
+++ app/socketio_ext.py
@@ -1,3 +1,7 @@
+import os
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(
+    cors_allowed_origins="*",
+    message_queue=os.getenv("REDIS_URL", "redis://redis:6379/0"),
+)
